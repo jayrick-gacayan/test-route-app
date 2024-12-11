82199b97157071b30b8c83a4f567f7326ed4b653
henryEguia <henry.eguia@kodakollectiv.com>
2024-12-11 12:30:00 +0800

    test: test parallel route link

--- app/[locale]/@testpar/profile-list/page.tsx
+++ app/[locale]/@testpar/profile-list/page.tsx
@@ -1,3 +1,10 @@
+import { Link } from '@/i18n/routing'
+
 export default function Page() {
-  return <div>Test Page test par profile-list</div>
+  return (
+    <div>
+      Test Page test par profile-list
+      <Link href='/profile-list/1'>test link</Link>
+    </div>
+  )
 }
