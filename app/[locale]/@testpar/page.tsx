a9de022b8aca4b7e575f6cbf38dc9836f5a25745
henryEguia <henry.eguia@kodakollectiv.com>
2024-12-11 12:25:23 +0800

    test: test link

--- app/[locale]/@testpar/page.tsx
+++ app/[locale]/@testpar/page.tsx
@@ -1,3 +1,10 @@
+import { Link } from '@/i18n/routing'
+
 export default function Page() {
-  return <div>Test Page test par</div>
+  return (
+    <div>
+      Test Page test par
+      <Link href='/profile-list/1'>test link</Link>
+    </div>
+  )
 }
