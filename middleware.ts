54fab9a12d250c6271fd3c0c5c56ddd27668bec1
henryEguia <henry.eguia@kodakollectiv.com>
2024-12-11 11:28:21 +0800

    test: test middleware

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,9 @@ export default createMiddleware({ ...routing, localePrefix: 'as-needed' })
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/((?!api|_next|_vercel|.*\\..*).*)'],
+  matcher: [
+    '/',
+    '/(de|en)/:path*',
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)',
+  ],
 }
