bf3031b5e40c4c2cbe79efcfe715b84c19f046ab
henryEguia <henry.eguia@kodakollectiv.com>
2024-12-11 13:27:44 +0800

    fix: added config for middleware

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,11 @@ import { routing } from './i18n/routing'
 
 // export default createMiddleware({ ...routing, localePrefix: 'as-needed' })
 
-// export const config = {
-//   // Match only internationalized pathnames
-//   matcher: [
-//     '/',
-//     '/(de|en)/:path*',
-//     '/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)',
-//   ],
-// }
+export const config = {
+  // Match only internationalized pathnames
+  matcher: [
+    '/',
+    '/(de|en)/:path*',
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)',
+  ],
+}
