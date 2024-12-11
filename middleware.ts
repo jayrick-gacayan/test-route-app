5ef1dd6b3f752642b99dce72c08b697d145ded74
henryEguia <henry.eguia@kodakollectiv.com>
2024-12-11 13:23:51 +0800

    test: test middleware

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,13 @@
 import createMiddleware from 'next-intl/middleware'
 import { routing } from './i18n/routing'
 
-export default createMiddleware({ ...routing, localePrefix: 'as-needed' })
+// export default createMiddleware({ ...routing, localePrefix: 'as-needed' })
 
-export const config = {
-  // Match only internationalized pathnames
-  matcher: [
-    '/',
-    '/(de|en)/:path*',
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)',
-  ],
-}
+// export const config = {
+//   // Match only internationalized pathnames
+//   matcher: [
+//     '/',
+//     '/(de|en)/:path*',
+//     '/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)',
+//   ],
+// }
